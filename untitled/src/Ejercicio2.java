e5b213cf7c4c25f829517e0e75fab586643b7141
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-08-19 10:20:25 -0500

    segundo ejercicio de Método void con parámetros

---
+++ untitled/src/Ejercicio2.java
@@ -0,0 +1,9 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        saludarEstudiante("Juan", 20);
+    }
+    public static void saludarEstudiante(String nombre, int edad) {
+        System.out.println("Hola " + nombre + ", tienes " + edad + " años.");
+    }
+}
