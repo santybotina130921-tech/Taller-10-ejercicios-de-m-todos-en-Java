bdd3ee3fe65fc5e371d09b5c00f1c6569438d7b6
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-08-19 10:15:58 -0500

    primer ejercicio se trata de Método void sin parámetros

---
+++ untitled/src/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+
+        public static void main(String[] args) {
+            mostrarBienvenida();
+        }
+        public static void mostrarBienvenida() {
+            System.out.println("¡Bienvenido al Instituto de Programación!");
+        }
+
+
+    }
